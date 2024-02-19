32c46d05e983b4c50117bae8376ca38f0a72b337
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-19 17:55:34 +0100

    Routing works

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Nav } from './components/Nav'
+import { Outlet } from 'react-router-dom'
 
 function App() {
     return (
         <>
             <Nav />
+            <main className="max-h-full p-4">
+                <Outlet />
+            </main>
         </>
     )
 }
