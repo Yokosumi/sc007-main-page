d26463ac2271aebb647236b6bf3b134afda1bf64
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-27 00:15:27 +0100

    Update styles in App and WelcomePage components

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -2,7 +2,7 @@ export const WelcomePage = () => {
     return (
         <>
             <h1>Welcome to the Welcome Page</h1>
-            <p className="selection:bg-indigo-700 selection:text-white">
+            <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
                 nobis blanditiis illo rerum, nulla laboriosam sint velit
                 suscipit ut assumenda!
