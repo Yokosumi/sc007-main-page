588c3c12d3ae68fec05e00fc489eb53de2cd485c
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-13 21:09:00 +0100

    test change for Github Actions

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -2,11 +2,6 @@ export const WelcomePage = () => {
     return (
         <>
             <h1>Welcome to the Welcome Page</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-                nobis blanditiis illo rerum, nulla laboriosam sint velit
-                suscipit ut assumenda!
-            </p>
         </>
     )
 }
