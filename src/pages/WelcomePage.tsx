0f2129a0abb9c5252cc2a9e300f53051162c5e1c
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-13 21:51:16 +0100

    hopefully the last test commit

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -2,8 +2,6 @@ export const WelcomePage = () => {
     return (
         <>
             <h1>Welcome to the Welcome Page</h1>
-            <p>testo pesto</p>
-            <p>testo pesto2</p>
         </>
     )
 }
